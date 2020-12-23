a627734d22a32d4bb790d439c4cfeffd30b73c01
feroz-appiness <feroz@appinessworld.com>
2020-12-23 09:49:34 +0530

    under construction complete

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Header from './Header'
+
+
+const App = () => {
+  return (
+    <div className='App-container'>
+   
+      <Header headingText='FLEXIBEES SITE UNDER CONSTRUCTION'/>
+    </div>
+  )
+}
+
+export default App
