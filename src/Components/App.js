d402b3c2038a0498764ab71cda3fb95bdb47ac04
feroz-appiness <feroz@appinessworld.com>
2020-12-23 11:41:12 +0530

    under construction complete

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import UnderConstruction from './underConstruction'
+
+
+
+const App = () => {
+  return (
+    <div >
+   <UnderConstruction/>
+      
+    </div>
+  )
+}
+
+export default App
